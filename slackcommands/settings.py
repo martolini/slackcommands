86ef6c123459207e67c69f03ea8d665153e9f330
Martin Skow Røed <msroed@gmail.com>
2015-07-14 13:24:38 +0200

    Fixed statics

--- slackcommands/settings.py
+++ slackcommands/settings.py
@@ -117,11 +117,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
-
-STATIC_URL = '/static/'
-
 #SLACKCOMMANDS
 SLACKCOMMANDS = [
     {
