e151483e6002fc7cc4fa3114471f201aa9dcd124
Martin Skow Røed <msroed@gmail.com>
2015-07-14 14:08:57 +0200

    Added /postfeature

--- commands/models.py
+++ commands/models.py
@@ -3,7 +3,11 @@ from django.conf import settings
 import requests
 
 class TrelloClient(object):
-	listids = {'bug': '54dcd88be020659f6bfacf14', 'ideas': '54d92005365133026da94f95'}
+	listids = {
+		'bug': '54dcd88be020659f6bfacf14', 
+		'ideas': '54d92005365133026da94f95',
+		'features': '54d920076de332204a69e29d'
+	}
 	def_payload = {'key': '591b45c0cbfe4b611a72e7984a482b9d', 'token': 'da4de1ca6da96cc5d1253079ed822dde1f6d5411644cc424b8501e4300c70922', 'idBoard': '54d91fa3170685886defba3f'}
 
 	@classmethod
