7eccb72160f26a441ea845093d1b6dabdf8cbef4
Martin Skow Røed <msroed@gmail.com>
2015-09-25 10:23:55 +0200

    Filter daily updates on worker/day

--- progresstracker/admin.py
+++ progresstracker/admin.py
@@ -5,6 +5,8 @@ from .models import Team, Worker, DailyUpdate
 class TeamAdmin(admin.ModelAdmin):
   list_display = ('id', 'name')
 
+
 @admin.register(DailyUpdate)
 class DailyUpdateAdmin(admin.ModelAdmin):
   list_display = ('worker', 'created_at')
+  list_filter = ('worker', 'created_at')
